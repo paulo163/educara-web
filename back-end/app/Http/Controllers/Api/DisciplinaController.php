a6397992bf15809cd026feb1d8991e8bd923ed35
ViniciusQueiroz-VQ <viniciusqueiroz.voq@gmail.com>
2020-05-30 23:28:49 -0300

    contrucao da API de Disciplinas

---
+++ back-end/app/Http/Controllers/Api/DisciplinaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Disciplina;
+use Illuminate\Http\Request;
+
+class DisciplinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Disciplina::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Disciplina::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Disciplina  $disciplina
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Disciplina $disciplina)
+    {
+        return Disciplina::findOrFail($disciplina->id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Disciplina  $disciplina
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Disciplina $disciplina)
+    {
+        $disciplina->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Disciplina  $disciplina
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Disciplina $disciplina)
+    {
+        $disciplina->delete();
+    }
+}
---
+++ back-end/app/Models/Disciplina.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Disciplina extends Model
+{
+    protected $fillable = [
+        'nome', 'imagem'
+    ];
+}
